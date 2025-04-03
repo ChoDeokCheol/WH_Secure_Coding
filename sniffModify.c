f454f2719801e56cb7054ef32c1b69d12816044f
ChoDeokCheol <gichan04@gmail.com>
2025-04-03 23:51:44 +0900

    Create sniffModify.c

---
+++ sniffModify.c
@@ -0,0 +1,111 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <pcap.h>
+#include <arpa/inet.h>
+
+/* Ethernet header */
+struct ethheader {
+  u_char  ether_dhost[6]; /* destination host address */
+  u_char  ether_shost[6]; /* source host address */
+  u_short ether_type;     /* protocol type (IP, ARP, RARP, etc) */
+};
+
+/* IP Header */
+struct ipheader {
+  unsigned char      iph_ihl:4, //IP header length
+                     iph_ver:4; //IP version
+  unsigned char      iph_tos; //Type of service
+  unsigned short int iph_len; //IP Packet length (data + header)
+  unsigned short int iph_ident; //Identification
+  unsigned short int iph_flag:3, //Fragmentation flags
+                     iph_offset:13; //Flags offset
+  unsigned char      iph_ttl; //Time to Live
+  unsigned char      iph_protocol; //Protocol type
+  unsigned short int iph_chksum; //IP datagram checksum
+  struct  in_addr    iph_sourceip; //Source IP address
+  struct  in_addr    iph_destip;   //Destination IP address
+};
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header,
+                              const u_char *packet)
+{
+  struct ethheader *eth = (struct ethheader *)packet;
+
+  if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
+    struct ipheader * ip = (struct ipheader *)
+                           (packet + sizeof(struct ethheader)); 
+
+    printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));   
+    printf("         To: %s\n", inet_ntoa(ip->iph_destip));    
+
+    /* determine protocol */
+    switch(ip->iph_protocol) {                                 
+        case IPPROTO_TCP:
+            printf("   Protocol: TCP\n");
+            return;
+        case IPPROTO_UDP:
+            printf("   Protocol: UDP\n");
+            return;
+        case IPPROTO_ICMP:
+            printf("   Protocol: ICMP\n");
+            return;
+        default:
+            printf("   Protocol: others\n");
+            return;
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  pcap_t *handle;
+  char errbuf[PCAP_ERRBUF_SIZE];
+  struct bpf_program fp;
+  char filter_exp[] = "icmp";
+  bpf_u_int32 net, mask;
+  char *dev;
+
+  // Step 1: 인터페이스 설정 (명령줄 인자 또는 자동 감지)
+  if (argc == 2) {
+      dev = argv[1];  // 사용자가 인터페이스 입력했을 경우
+  } else {
+      dev = pcap_lookupdev(errbuf);  // 자동으로 네트워크 인터페이스 찾기
+      if (dev == NULL) {
+          fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+          return EXIT_FAILURE;
+      }
+  }
+  printf("Using device: %s\n", dev);
+
+  // Step 2: 네트워크 주소 및 마스크 가져오기
+  if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
+      fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
+      net = 0;
+      mask = 0;
+  }
+
+  // Step 3: 패킷 캡처 핸들 열기
+  handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+  if (handle == NULL) {
+      fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+      return EXIT_FAILURE;
+  }
+
+  // Step 4: 필터 설정 (ICMP 패킷만 캡처)
+  if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+      fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
+      return EXIT_FAILURE;
+  }
+  if (pcap_setfilter(handle, &fp) == -1) {
+      fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
+      return EXIT_FAILURE;
+  }
+
+  printf("Capturing ICMP packets on %s...\n", dev);
+
+  // Step 5: 패킷 캡처 시작
+  pcap_loop(handle, -1, got_packet, NULL);
+
+  pcap_close(handle);   // Close the handle
+  return EXIT_SUCCESS;
+}
